631637c6106847eb9f70a4123959475919ae7c15
Kasen's Desktop <kasenb1211@gmail.com>
2023-03-02 22:44:49 -0500

    created projects component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
+import Projects from "./Projects";
+import Footer from "./components/Footer";
 // import AboutMe from "./components/AboutMe";
 // import Portfolio from "./components/Portfolio";
 // import ContactMe from "./components/ContactMe";
-// import Projects from "./Projects";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <Header />
+      <Projects />
       <Footer />
     </div>
   );
