6593d220eb99c77887e063bb45c23529deff7913
Kasen's Desktop <kasenb1211@gmail.com>
2023-03-04 22:18:15 -0500

    finished up Projects component and added assets folder

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,3 +1,24 @@
-export default function Project() {
-  return <div></div>;
-}
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+
+const Project = (props) => {
+  const { title, image, description, deployedLink, githubLink } = props;
+
+  return (
+    <Card className="mt-3 mb-5 mx-3 p-1">
+      <Card.Img variant="top" src={image} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+        <Button variant="success" href={deployedLink} target="_blank">
+          Deployed Link
+        </Button>{" "}
+        <Button variant="dark" href={githubLink} target="_blank">
+          GitHub Link
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Project;
